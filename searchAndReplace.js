e6e548aa873719c35e4784390fb1ee5e7be3473c
nikolasbo <niktechnopro@gmail.com>
2021-10-08 08:17:39 -0400

    updated with second option

--- searchAndReplace.js
+++ searchAndReplace.js
@@ -18,7 +18,19 @@ function myReplace(str, before, after){
   else{
     after = after.slice(0,1).toLowerCase() + after.slice(1);
   }
+
+  //split string and convert into array
+  let modifiedStr = str.split(" ");
+  console.log(modifiedStr)
+  if(modifiedStr.includes(before)){
+    let idx = modifiedStr.indexOf(before);
+    // modify an array
+    modifiedStr[idx] = after;
+  }
+  // convert back to string and return
+  return modifiedStr.join(" ");
 }
+
 
 
 
