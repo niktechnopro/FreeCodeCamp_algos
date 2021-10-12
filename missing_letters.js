1312d7e370ada78e45992c24e1407b1284519e7e
nikolasbo <niktechnopro@gmail.com>
2021-10-11 20:41:39 -0400

    missing letters
    body
    missing letter with as usual 2 options

---
+++ missing_letters.js
@@ -0,0 +1,18 @@
+function fearNotLetter(str) {
+  // const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+  //Convert a Unicode number into a character
+  const alphabet = alpha.map((x) => String.fromCharCode(x).toLocaleLowerCase());
+  //find a character in alphabet - I'll use the last letter of str
+  const firstChar = str.charAt(0);
+  const lastChar = str.charAt(str.length - 1);
+  //make a helper array in range of first and last character
+  //we need to find indexes for firstChar and lastChar in alphabet
+  const firstIndex = alphabet.indexOf(firstChar);
+  const lastIndex = alphabet.indexOf(lastChar);
+  const tempAlphabetHelper = alphabet.slice(firstIndex, lastIndex+1);
+  var difference = tempAlphabetHelper.filter(x => !str.split("").includes(x));
+  return difference[0];
+}
+
+fearNotLetter("abce");
