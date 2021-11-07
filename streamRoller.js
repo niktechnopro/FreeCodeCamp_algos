ac131508ffb582b0ebee48bb1bd1126a4fe7b4a3
nikolasbo <niktechnopro@gmail.com>
2021-11-07 16:18:36 -0500

    added second - more consice option

--- streamRoller.js
+++ streamRoller.js
@@ -7,21 +7,37 @@ function steamrollArray(arr) {
         // console.log("here: ", el)
         if(Array.isArray(el)){//if array
             for(let i = 0; i < el.length; i++){
-            if(Array.isArray(el[i])){
-                flattenSimpleArray(el[i]);
-            }
-            else{
-                temp.push(el[i]);
-            }
+                if(Array.isArray(el[i])){
+                    flattenSimpleArray(el[i]);
+                }
+                else{
+                    temp.push(el[i]);
+                }
             }
         }
         else{//if not an array
             temp.push(el);
         }
-        return temp;
     }
     for(let el of arr){
         flattenSimpleArray(el);
+    }
+    return temp;
+}
+
+
+//option 2
+function steamrollArray(arr) {
+    const temp = [];
+    // Loop over array contents
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        // Recursively flatten entries that are arrays and push into the temp
+        temp.push(...steamrollArray(arr[i]));
+      } else {
+        // Copy contents that are not arrays
+        temp.push(arr[i]);
+      }
     }
     return temp;
 }
